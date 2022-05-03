c7bbdf44561b0e2afcbd56d15476422d10cdfefa
Kim BeomJung <kbjoong@daum.net>
2022-05-03 20:16:52 +0900

    Delete test_pathlib.py

--- Crawling/music/test_pathlib.py
+++
@@ -1,8 +0,0 @@
-import os
-
-path = os.path.dirname(__file__).replace('\\', '/')
-print(type(path), path)
-# jupyter notebook 에서는 __file__
-
-crawled_folder = path + '/crawled_data/test_data'
-os.makedirs(crawled_folder, exist_ok=True)
